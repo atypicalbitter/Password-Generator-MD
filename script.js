2dcb079a9b7feb3dccd746d0d11d443b784066ab
Matt Dudman <mattdudman95@googlemail.com>
2023-12-17 18:13:46 +0000

    Issue found in the code, worked on it in seperate repo and used research to correct my mistake.

--- script.js
+++ script.js
@@ -88,7 +88,6 @@ const upperCasedCharacters = [
 function getPasswordOptions() {
   let length = parseInt(prompt("Enter the length of your password (between 8 and 128 characters):"));
 
-
   while (isNaN(length) || length < 8 || length > 128) {
     alert("Please enter a valid number between 8 and 128.");
     length = parseInt(prompt("Enter the length of your password (between 8 and 128 characters):"));
@@ -98,7 +97,6 @@ function getPasswordOptions() {
   let hasUppercase = confirm("Include uppercase characters?");
   let hasNumeric = confirm("Include numeric characters?");
   let hasSpecial = confirm("Include special characters?");
-
 
   while (!(hasLowercase || hasUppercase || hasNumeric || hasSpecial)) {
     alert("At least one character type must be selected.");
@@ -122,44 +120,40 @@ function getRandom(arr) {
   return arr[randomIndex];
 }
 
-// Function to generate password with user input
 function generatePassword() {
   const options = getPasswordOptions();
   let possibleCharacters = [];
   let result = "";
-}
-
-if (options.hasLowercase) {
-  possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
-}
-if (options.hasUppercase) {
-  possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
-}
-if (options.hasNumeric) {
-  possibleCharacters = possibleCharacters.concat(numericCharacters);
-}
-if (options.hasSpecial) {
-  possibleCharacters = possibleCharacters.concat(specialCharacters);
-}
-
-for (let i = 0; i < options.length; i++) {
-  const randomCharacter = getRandom(possibleCharacters);
-  result += randomCharacter;
-}
-
-return result;
 
 
-// Get references to the #generate element
+  if (options.hasLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+  }
+  if (options.hasUppercase) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+  }
+  if (options.hasNumeric) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+  }
+  if (options.hasSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
+
+
+  for (let i = 0; i < options.length; i++) {
+    const randomCharacter = getRandom(possibleCharacters);
+    result += randomCharacter;
+  }
+
+  return result;
+}
+
 const generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener('click', writePassword);
 
-// Write password to the #password input
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
